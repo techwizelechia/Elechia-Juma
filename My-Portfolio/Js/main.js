43dc01e9a3b511b154fd317da7a82fdb3aac8d74
Elechia Juma <otienoelechia3@gmail.com>
2025-06-06 20:31:25 +0300

    add project section and style

--- My-Portfolio/Js/main.js
+++ My-Portfolio/Js/main.js
@@ -10,3 +10,21 @@ hamburger.addEventListener('click', () => {
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('open');
 });
+
+// Show button when user scrolls down
+window.onscroll = function () {
+  const btn = document.getElementById("backToTopBtn");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+// Scroll to top function
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
